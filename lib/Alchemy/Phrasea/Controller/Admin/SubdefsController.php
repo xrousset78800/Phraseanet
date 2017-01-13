5e9d5ee14956465e9cbbfd9c61b0dbafbb1e8fc3
Xavier Rousset <xrousset78800@gmail.com>
2017-01-13 18:57:40 +0100

    Update admin subdef setup

--- lib/Alchemy/Phrasea/Controller/Admin/SubdefsController.php
+++ lib/Alchemy/Phrasea/Controller/Admin/SubdefsController.php
@@ -23,9 +23,91 @@ class SubdefsController extends Controller
     function indexAction($sbas_id) {
         $databox = $this->findDataboxById((int) $sbas_id);
 
+        $config = array(
+            "image" => array(
+                "definitions" => array(
+                    "low_I"    => array("250", "75", "no", "no", "25", "tiff", ["print"]),
+                    "medium_I" => array("500", "150", "no", "yes", "50", "png", ["screen", "print"]),
+                    "high_I" => array("750", "250", "yes", "yes", "75", "jpeg", ["all"]),
+                ),
+
+                "form" => array(
+                    "size" => "slide",
+                    "resolution" => "slide",
+                    "strip" => "radio",
+                    "flatten" => "radio",
+                    "quality" => "slide",
+                    "icodec" => "select",
+                    "devices" => "checkbox",
+                ),
+            ),
+            "video" => array(
+                "definitions" => array(
+                    "low_V"    => array("50", "8000", "2000", "20", "200", "20", "libx264", "libfaac", ["screen"]),
+                    "medium_V" => array("120", "50000", "7500", "140", "600", "100", "libvpx", "libmp3lame", ["print", "projection"]),
+                    "high_V" => array("200", "96000", "10000", "180", "1100", "150", "libtheora", "libvorbis", ["all"]),
+                ),
+
+                "form" => array(
+                    "audiobitrate" => "slide",
+                    "audiosamplerate" => "select",
+                    "bitrate" => "slide",
+                    "GOPsize" => "slide",
+                    "size" => "slide",
+                    "fps" => "slide",
+                    "vcodec" => "select",
+                    "acodec" => "select",
+                    "devices" => "checkbox",
+                ),
+            ),
+            "gif" => array(
+                "definitions" => array(
+                    "low_G"    => array("800", "75", "no", "no", "20", "jpeg", "120", ["tv"]),
+                    "medium_G" => array("1200", "150", "yes", "no", "60", "png", "250", ["screen", "handheld"]),
+                    "high_G" => array("1500", "220", "yes", "yes", "90", "tiff", "420", ["all"]),
+                ),
+
+                "form" => array(
+                    "size" => "slide",
+                    "resolution" => "slide",
+                    "strip" => "radio",
+                    "flatten" => "radio",
+                    "quality" => "slide",
+                    "icodec" => "select",
+                    "delay" => "slide",
+                    "devices" => "checkbox",
+                ),
+            ),
+            "audio" => array(
+                "definitions" => array(
+                    "low_A"    => array("100", "8000", "libmp3lame", ["tv"]),
+                    "medium_A" => array("180", "44100", "flac", ["screen, print"]),
+                    "high_A" => array("230", "50000", "libmp3lame", ["all"]),
+                ),
+
+                "form" => array(
+                    "audiobitrate" => "slide",
+                    "audiosamplerate" => "select",
+                    "acodec" => "select",
+                    "devices" => "checkbox",
+                ),
+            ),
+            "flexpaper" => array(
+                "definitions" => array(
+                    "low_F"    => array(),
+                    "medium_F" => array(),
+                    "high_F" => array(),
+                ),
+
+                "form" => array(
+                ),
+            ),
+        );
+
         return $this->render('admin/subdefs.html.twig', [
             'databox' => $databox,
-            'subdefs' => $databox->get_subdef_structure()
+            'subdefs' => $databox->get_subdef_structure(),
+            'config' => $config
         ]);
     }
 
